edaf2a8e847507e7049987164f343ef49413930b
Arun Kumar <90699610+ArunnKummar@users.noreply.github.com>
2024-07-25 18:23:01 +0530

    Update buttonControl.js

--- SAP UI5 Fiori/fiori_like_app/controls/buttonControl.js
+++ SAP UI5 Fiori/fiori_like_app/controls/buttonControl.js
@@ -1,1 +1,15 @@
-
+sap.ui.define([],function(){
+	sap.m.Button.extend("fiorilike.controls.buttonControl",{
+		metadata:{
+			events:{
+				"onUp":{}
+			}
+			
+		},
+		init:function(){},
+		renderer:{},
+		onmouseover:function(){
+			this.fireOnUp();
+		}
+	});
+});
